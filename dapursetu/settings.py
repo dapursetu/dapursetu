3195518afcf530c58a73ecf78ff99c55b50a7c92
Dapur Setu <dapursetu@icloud.com>
2021-08-02 19:42:26 +0700

    07.42

--- dapursetu/settings.py
+++ dapursetu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--e)ap2340gs+c(ss6g=0gsa(v@0q)=#-*b7#o&r%tb!3a@szh4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dapursetu.pythonanywhere.com']
 
 
 # Application definition
